3a292903758293ecc2908100c0740728929ee86f
unknown <rlaalsrl9569@naver.com>
2023-03-28 21:03:53 +0900

    댓글 구현중, CORS 설정

---
+++ src/main/java/com/practice/board/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.practice.board.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST")
+                .allowCredentials(true);
+    }
+}
